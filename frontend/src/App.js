8f6d69a5592fdbad67f4218b2ae861dd7711b65e
rnewso4 <54243809+rnewso4@users.noreply.github.com>
2022-12-02 20:46:42 -0600

    another bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import Login from './Components/Login'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom'
 import Register from './Components/Register';
 import Homepage from './Components/Homepage';
 import Friends from './Components/Friends';
@@ -7,15 +7,15 @@ import ManageEvent from './Components/ManageEvent';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <>
         <Routes>
           <Route path="/VolleyProject" element={ <Login />}/>
-          {/* <Route path="/VolleyProject/login" element={ < Login />}/>
+          <Route path="/VolleyProject/login" element={ < Login />}/>
           <Route path="/VolleyProject/register" element={ < Register />}/>
           <Route path="/VolleyProject/homepage" element={ < Homepage />}/>
           <Route path="/VolleyProject/friends" element={ < Friends />}/>
-          <Route path="/VolleyProject/eventmanagement" element={ < ManageEvent />}/> */}
+          <Route path="/VolleyProject/eventmanagement" element={ < ManageEvent />}/>
 
           <Route path="/" exact element={ <Login />}/>
           <Route path="/login" element={ < Login />}/>
@@ -25,7 +25,7 @@ function App() {
           <Route path="/eventmanagement" element={ < ManageEvent />}/>
         </Routes>
       </>
-    </Router>
+    </HashRouter>
   );
 }
 
